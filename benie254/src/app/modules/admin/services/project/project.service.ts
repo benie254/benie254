8a3a6695f5f67622bfb1b4df0fe652582dbed26e
benie254 <davinci.monalissa@gmail.com>
2022-12-30 02:55:25 +0300

    remove unused service && clear project service

--- benie254/src/app/modules/admin/services/project/project.service.ts
+++ benie254/src/app/modules/admin/services/project/project.service.ts
@@ -4,23 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ProjectService {
-  projects: any[] = [];
-
-  add(data: any){
-    this.projects.push(data)
-    this.setObj('myProjects',JSON.stringify(data))
-    console.warn("my projects",this.getObj('myProjects'))
-  }
-  setObj = function(key, obj) {
-    return this.setItem(key, JSON.stringify(obj))
-  }
-  getObj = function(key) {
-      return JSON.parse(this.getItem(key))
-  }
-  clear(){
-    this.projects = [];
-  }
-  
 
   constructor() { }
 }
