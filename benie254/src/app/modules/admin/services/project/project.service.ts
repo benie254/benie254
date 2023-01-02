7f8def0b21ce0ebba4cabf97116529ea5a4f2fd0
benie254 <davinci.monalissa@gmail.com>
2023-01-02 14:25:29 +0300

    create new service requests && update their urls

--- benie254/src/app/modules/admin/services/project/project.service.ts
+++ benie254/src/app/modules/admin/services/project/project.service.ts
@@ -10,7 +10,15 @@ const api = 'http://127.0.0.1:8000/api/';
 })
 export class ProjectService {
   addProj = api + 'admin/projects/add/';
-  updateProj = api + 'admin/project/update/';
+  updateProj = api + 'admin/project/details/';
+  allFeeds = api + 'admin/feedbacks/all/';
+  feedDet = api + 'admin/feedback/details';
+  allReacts = api + 'admin/reactions/all/';
+  reactDet = api + 'admin/reaction/details';
+  allTechs = api + 'admin/technologies/all/';
+  techDet = api + 'admin/feedback/details';
+  allMsgs = api + 'admin/contacts/all/';
+  msgDet = api + 'admin/contact/details';
 
   constructor(
     private handler:ReqHandlerService,
@@ -25,6 +33,64 @@ export class ProjectService {
   deleteProject(id: number): Observable<any>{
     return this.handler.handleDEL<any>(this.updateProj + id);
   }
-
-
+  getAllTechnologies(): Observable<any>{
+    return this.handler.handleGET<any>(this.allTechs);
+  }
+  postTechnology(data: any): Observable<any>{
+    return this.handler.handlePOST<any>(this.allTechs, data);
+  }
+  getTechnologyDetails(): Observable<any>{
+    return this.handler.handleGET<any>(this.techDet);
+  }
+  editTechnology(id:number, data: any): Observable<any>{
+    return this.handler.handlePUT<any>(this.techDet + id, data);
+  }
+  deleteTechnology(id: number): Observable<any>{
+    return this.handler.handleDEL<any>(this.techDet + id);
+  }
+  getAllFeedbacks(): Observable<any>{
+    return this.handler.handleGET<any>(this.allFeeds);
+  }
+  postFeedback(data: any): Observable<any>{
+    return this.handler.handlePOST<any>(this.allFeeds, data);
+  }
+  getFeedbackDetails(): Observable<any>{
+    return this.handler.handleGET<any>(this.feedDet);
+  }
+  editFeedback(id:number, data: any): Observable<any>{
+    return this.handler.handlePUT<any>(this.feedDet + id, data);
+  }
+  deleteFeedback(id: number): Observable<any>{
+    return this.handler.handleDEL<any>(this.feedDet + id);
+  }
+  getAllReactions(): Observable<any>{
+    return this.handler.handleGET<any>(this.allReacts);
+  }
+  postReaction(data: any): Observable<any>{
+    return this.handler.handlePOST<any>(this.allReacts, data);
+  }
+  getReactionDetails(): Observable<any>{
+    return this.handler.handleGET<any>(this.reactDet);
+  }
+  editReaction(id:number, data: any): Observable<any>{
+    return this.handler.handlePUT<any>(this.reactDet + id, data);
+  }
+  deleteReaction(id: number): Observable<any>{
+    return this.handler.handleDEL<any>(this.reactDet + id);
+  }
+  getAllContacts(): Observable<any>{
+    return this.handler.handleGET<any>(this.allMsgs);
+  }
+  postContact(data: any): Observable<any>{
+    return this.handler.handlePOST<any>(this.allMsgs, data);
+  }
+  getContactDetails(): Observable<any>{
+    return this.handler.handleGET<any>(this.msgDet);
+  }
+  editContact(id:number, data: any): Observable<any>{
+    return this.handler.handlePUT<any>(this.msgDet + id, data);
+  }
+  deleteContact(id: number): Observable<any>{
+    return this.handler.handleDEL<any>(this.msgDet + id);
+  }
 }
--- benie254/src/app/services/projects/my-projects.service.ts
+++ benie254/src/app/services/projects/my-projects.service.ts
@@ -11,6 +11,9 @@ const api = 'http://127.0.0.1:8000/api/';
 export class MyProjectsService {
   allProjs = api + 'projects/all/';
   projDet = api + 'project/details/';
+  projFeeds = api + 'project/feedbacks/';
+  projReacts = api + 'project/reactions/';
+  projTechs = api + 'project/technologies/';
 
   constructor(
     private handler: ReqHandlerService,
@@ -21,5 +24,14 @@ export class MyProjectsService {
   }
   getProjectDetails(id: number): Observable<any>{
     return this.handler.handleGET<any>(this.projDet + id);
+  }
+  getProjectFeedbacks(id: number): Observable<any>{
+    return this.handler.handleGET<any>(this.projFeeds + id);
+  }
+  getProjectReactions(id: number): Observable<any>{
+    return this.handler.handleGET<any>(this.projReacts + id);
+  }
+  getProjectTechnologiess(id: number): Observable<any>{
+    return this.handler.handleGET<any>(this.projTechs + id);
   }
 }
